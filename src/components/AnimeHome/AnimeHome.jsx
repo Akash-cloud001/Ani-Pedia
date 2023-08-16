c920e2a6ff46d417cb20de3f65a180346022e7b9
Akash Parmar <ak.parmar1442000@gmail.com>
2023-08-17 01:16:33 +0530

    NextPageAndPrevPageImplementationIsDone

--- src/components/AnimeHome/AnimeHome.jsx
+++ src/components/AnimeHome/AnimeHome.jsx
@@ -2,22 +2,11 @@ import React, { useContext } from 'react'
 import styles from './AnimeHome.module.css';
 import AnimeCard from '../AnimeCard/AnimeCard';
 import { Link } from 'react-router-dom';
-import { DataContext } from '../../contexts/DataProvider';
+
 const PopularAnime = ({name, animeData}) => {   
-    const {fetchPopularData, fetchAiringData} = useContext(DataContext);
+
     const animes = animeData?.slice(0,8); 
-    function handleRedirectionData(){
-        switch(name){
-            case 'popular':
-                fetchPopularData();
-                break;
-            case 'airing':
-                fetchAiringData();
-                break;
-            default:
-                break;
-        }
-    }
+    
   return (
     <div className={styles.popular}>
         <h2 className={styles.header}>
@@ -32,7 +21,7 @@ const PopularAnime = ({name, animeData}) => {
                 />
             })}
         </div>
-        <Link className={styles.btn} to={`${name}/1`} onClick={handleRedirectionData}>
+        <Link className={styles.btn} to={`${name}/1`}>
             View More <i className="ri-arrow-right-line"></i>
         </Link>
     </div>
--- src/components/PopularAnime/PopularAnime.jsx
+++ src/components/PopularAnime/PopularAnime.jsx
@@ -1,20 +1,23 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { baseURL, filterURL } from '../../contexts/DataProvider';
 import styles from './PopularAnime.module.css';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Loading from '../Loader/Loading';
 import AnimeCard from '../AnimeCard/AnimeCard';
 import GoToTop from '../GoToTop/GoToTop';
 
 const PopularAnime = () => {
+  const [nextPage, setNextPage ]= useState(1);
   const {pageNum} = useParams();
+  const navigate = useNavigate();
+
   const [loader, setLoader] = useState(true);
   const [pageData, setPageData] = useState([]);
 
   async function fetchPopularData(pageNo = pageNum){
-
-    await axios.get(`${baseURL}${filterURL.popular}&page=${pageNo}`)
+    setLoader(true);
+    await axios.get(`${baseURL}${filterURL.popular}&page=${pageNum}`)
     .then((response)=>{
 
       const data = response.data;
@@ -26,21 +29,33 @@ const PopularAnime = () => {
     });
   }
 
+  function handleNextPage(){
+    //TODO
+    let temp = nextPage + 1;
+    setNextPage(prev=>prev+1);
+    navigate(`/popular/${temp}`);
+  }
+  function handlePrevPage(){
+    //TODO
+    let temp = nextPage - 1;
+    if(temp === 0) return;
+    setNextPage(prev=>prev-1);
+    navigate(`/popular/${temp}`);
+  }
+  
+  useEffect(()=>{
+    fetchPopularData(pageNum);
+  },[nextPage]);
 
   useEffect(()=>{
       console.log(`popular Data of page Num ${pageNum}`);
       fetchPopularData(pageNum);
+      window.addEventListener('popstate',()=>{
+          handlePrevPage();
+      });
+      
   },[]);
 
-  useEffect(()=>{
-    function handlePopState(){
-      fetchPopularData(pageNum);
-    }
-    window.addEventListener('popstate', handlePopState);
-    return()=>{
-      window.removeEventListener('popstate', handlePopState);
-    }
-  },[]);
 
   return (
     <>
@@ -60,13 +75,21 @@ const PopularAnime = () => {
           })}
         </main>
         <div className={styles.btnContainer}>
-          <button  className={styles.btn}>
+           <button  
+            className={styles.btn}
+            disabled={pageNum==='1'?true:false}
+            onClick={handlePrevPage}
+           >
             <i className="ri-arrow-left-s-line"></i>
             <span>Prev</span>
           </button>
-          <button className={styles.btn}>
-            <i className="ri-arrow-right-s-line"></i>
+         <button 
+            className={styles.btn} 
+            disabled={!pageData.pagination.has_next_page}
+            onClick={handleNextPage}
+          >
             <span>Next</span>
+            <i className="ri-arrow-right-s-line"></i>
           </button>
         </div>
         <GoToTop />
