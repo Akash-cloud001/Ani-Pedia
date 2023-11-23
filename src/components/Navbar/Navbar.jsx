d39c7bb14fbdde94b0cfe8efe1363177c76b455c
Akash Parmar <ak.parmar1442000@gmail.com>
2023-11-23 13:10:55 +0530

    implemented the search functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,86 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Navbar.module.css';
-const Navbar = () => {
-  const [text, setText] = useState('');
+import axios from 'axios';
+import { NavLink ,Link, useNavigate,redirect } from 'react-router-dom';
 
+const Navbar = () => {
+  const navigate = useNavigate();
+  const [query, setQuery] = useState('');
+  const [queryData, setQueryData] = useState([]);
   const handleSubmit = (e)=>{
+    search();
     e.preventDefault();
-    // call the api with the search string and redirect to the dedicated page of that particular anime
-    setText('');
   }
+  
+  function search(){
+    const baseUrl = ` https://api.jikan.moe/v4/anime?q=${query}&sfw`;
+    if(query!==''){
+      axios.get(baseUrl)
+      .then((res)=>{
+        setQueryData(res.data.data);
+      })
+      .catch((err)=>{
+        console.log(err);
+        setQuery('');
+      })  
+    }else{
+      setQueryData([]);
+    }
+  }
+
+  const handleInputChange= (event)=>{
+    setQuery(event.target.value);
+  }
+
+  useEffect(()=>{
+    let timeOut = setTimeout(()=>{
+      search();
+    },1000);
+    return ()=>clearTimeout(timeOut);
+  },[query]);
+
+
+  useEffect(()=>{
+    window.addEventListener("popstate",()=>{
+      location.reload();
+    });
+  },[]);
 
   return (
     <div className={styles.nav}>
       <header className={styles.header}>
         <span>ANI</span>&nbsp;<span>PEDIA</span>
       </header>
-      <ul className={styles.ul}>
-        <li className={styles.li}>
-          <form className={styles.form} onSubmit={handleSubmit}>
-            <input className={styles.input} type='text' placeholder='Search anime...' value={text} onChange={(e)=>setText(e.target.value)}/>
-            <button className={styles.button} type='submit'>
-              <i className="ri-search-2-line"></i>
-            </button>
-          </form>
-        </li>
-      </ul>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input 
+          tabIndex={0} 
+          className={styles.input} 
+          type='text' 
+          placeholder='Search anime...' 
+          value={query}
+          onChange={handleInputChange}
+        />
+        <button className={styles.button} type='submit'>
+          <i className="ri-search-2-line"></i>
+        </button>
+        {query && queryData && <div className={styles.searchData}>
+          <div className={styles.searchUl}>
+            {queryData.map((element)=>{
+              return <li 
+                        key={element.mal_id} 
+                        className={styles.searchList} 
+                        onClick={()=>{
+                          const title = element.title_english !== null?element.title_english:element.title;
+                           navigate(`/single-anime/${title}/${element.mal_id}`);
+                           location.reload();
+                        }}
+                      >
+                        {element.title}
+                      </li>
+            })}
+          </div>
+        </div>}
+      </form>
     </div>
   )
 }
