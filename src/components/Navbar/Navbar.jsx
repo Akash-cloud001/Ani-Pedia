1c23f715434603e07dca5a53f524fcc2b948ef8c
Akash Parmar <ak.parmar1442000@gmail.com>
2023-11-26 16:39:27 +0530

    implementedErrorHandlingInSearch

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Navbar.module.css';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [query, setQuery] = useState('');
   const [queryData, setQueryData] = useState([]);
+  const [error, setError] = useState(false);
   const handleSubmit = (e)=>{
     search();
     e.preventDefault();
@@ -18,6 +19,10 @@ const Navbar = () => {
       axios.get(baseUrl)
       .then((res)=>{
         setQueryData(res.data.data);
+        console.log(res.data);
+        if(res.data.pagination.items.count === 0){
+          setError(true);
+        }
       })
       .catch((err)=>{
         console.log(err);
@@ -29,6 +34,7 @@ const Navbar = () => {
   }
 
   const handleInputChange= (event)=>{
+    setError(false);
     setQuery(event.target.value);
   }
 
@@ -63,24 +69,56 @@ const Navbar = () => {
         <button className={styles.button} type='submit'>
           <i className="ri-search-2-line"></i>
         </button>
-        {query && queryData && <div className={styles.searchData}>
-          <ul className={styles.searchUl}>
-            {queryData.map((element)=>{
-              return <li 
-                        key={element.mal_id} 
-                        className={styles.searchList} 
-                        onClick={()=>{
-                          const title = element.title_english !== null?element.title_english:element.title;
-                           navigate(`/single-anime/${title}/${element.mal_id}`);
-                           location.reload();
-                        }}
-                      >
-                        {element.title}
-                      </li>
-            })}
-          </ul>
-        </div>}
       </form>
+
+
+      {error ? 
+        <p className={styles.errorMessage}>No result found !!</p> 
+        : 
+        queryData.length!==null && <div className={styles.searchData}>
+        <ul className={styles.searchUl}>
+          {queryData.map((element)=>{
+            return <li 
+                      key={element.mal_id} 
+                      className={styles.searchList} 
+                      onClick={()=>{
+                        const title = element.title_english !== null?element.title_english:element.title;
+                          navigate(`/single-anime/${title}/${element.mal_id}`);
+                          location.reload();
+                      }}
+                    >
+                      <img className={styles.searchImg} src={element.images.webp.image_url} alt={element.title} />
+                      <div className={styles.searchResultContainer}>
+                        <h3 className={styles.searchHeader}>
+                          {element.title}
+                        </h3>
+                        
+                        <p className={styles.synopsis}>
+                          {element.synopsis}
+                        </p>
+                        <div className={styles.searchValues}>
+                          <span>
+                            {element.aired.string}
+                          </span>
+                          <span>
+                          <i className="ri-checkbox-blank-circle-fill"></i>
+                            {element.type}
+                          </span>
+                        </div>
+                      </div>
+                    </li>
+          })}
+        </ul>
+        {/* {!queryData.length ? null : 
+        <Link to={'/search-result/1'} className={styles.seeAllResults} onClick={()=>{setQuery(''); setQueryData([])}}>
+          See All Results
+        </Link>
+        
+        } */}
+        </div>
+      }
+
+      
     </div>
   )
 }
